6129b0ae4f6133b27350eb3b71b16dcf6cccc06d
b-spiral <Mitsuhiro.SHIBUYA@gmail.com>
2016-06-12 20:36:22 +0900

    データ構造改善。
    mapをソート済み配列に置き換え。

--- src/NodeSet.h
+++ src/NodeSet.h
@@ -1,6 +1,7 @@
 #pragma	once
 #include <vector>
 #include <map>
+#include <algorithm>
 
 class NodeSet
 {
@@ -29,53 +30,104 @@ public:
 	}
 	static	std::auto_ptr<NodeSet>	buildFromNodeList( std::map<int,int> *pMapNodeIndexToNodesetIndex, const std::vector<Node>& nodes)
 	{
+		std::auto_ptr<NodeSet>	pRet(new NodeSet);
+		NodeSet& ns = *pRet;
+
+		int	totalEntry = 0;
+		for (int i = 0; i < nodes.size(); i++) {
+			totalEntry += nodes[i].chrToNextIndex.size();
+		}
+
+		ns.chrIndexs.reserve(totalEntry);
+		ns.nds.resize( nodes.size() );
+		for (int i = 0; i < nodes.size(); i++) {
+			const Node& node = nodes[i];
+			Nd& nd = ns.nds[i];
+
+			nd.chrIndexBegin = ns.chrIndexs.size();
+			for (std::map<chr_t, int>::const_iterator it = node.chrToNextIndex.begin(), itE = node.chrToNextIndex.end(); it != itE; it++) {
+				ChrIndex	ci;
+				ci.chr = it->first;
+				ci.i = it->second;
+				ns.chrIndexs.push_back(ci);
+			}
+			nd.chrIndexEnd = ns.chrIndexs.size();
+
+			nd.depth = node.depth;
+			nd.failIndex = node.failIndex;
+			nd.matchIndex = node.matchIndex;
+			nd.matchPatternno = node.matchPatternno;
+		}
+
 		if (pMapNodeIndexToNodesetIndex) {
 			pMapNodeIndexToNodesetIndex->clear();
 			for (int i = 0; i < nodes.size(); i++) {
 				pMapNodeIndexToNodesetIndex->insert(std::make_pair(i, i));
 			}
 		}
-		return	std::auto_ptr<NodeSet>(new NodeSet(nodes));
+		return	pRet;
 	}
 
 private:
-	explicit	NodeSet(const std::vector<Node>& nodes_)
-		: nodes(nodes_)
+	NodeSet()
 	{}
 
 private:
-	std::vector<Node>	nodes;
+	struct ChrIndex
+	{
+		chr_t	chr;
+		int		i;
+	};
+	std::vector<ChrIndex>	chrIndexs;
+	
+	struct ComparatorChrIndexChrLess
+	{
+		inline	bool	operator()(const ChrIndex& ci, const chr_t& chr)	const	{	return	ci.chr < chr;	}
+	};
+
+	struct Nd
+	{
+		int	chrIndexBegin, chrIndexEnd;
+		int	matchPatternno;
+		int	depth;
+		int	failIndex;
+		int	matchIndex;
+	};
+	std::vector<Nd>	nds;
 
 public:
 	std::pair<bool, int>	searchNext(int index, chr_t chr)	const
 	{
-		const Node&	node = nodes[index];
-		std::map<chr_t, int>::const_iterator it = node.chrToNextIndex.find(chr);
-		if (it != node.chrToNextIndex.end()) {
-			return	std::make_pair(true, it->second);
-		}
-		else {
+		const Nd& nd = nds[index];
+		std::vector<ChrIndex>::const_iterator itB = chrIndexs.begin();
+		std::advance(itB, nd.chrIndexBegin);
+		std::vector<ChrIndex>::const_iterator itE = chrIndexs.begin();
+		std::advance(itE, nd.chrIndexEnd);
+		std::vector<ChrIndex>::const_iterator it = std::lower_bound(itB, itE, chr, ComparatorChrIndexChrLess());
+		if ( it != itE && it->chr == chr) {
+			return	std::make_pair(true, it->i);
+		}else{
 			return	std::make_pair(false, -1);
 		}
 	}
 	int	getMatchIndex(int index)	const
 	{
-		const Node&	node = nodes[index];
-		return	node.matchIndex;
+		const Nd& nd = nds[index];
+		return	nd.matchIndex;
 	}
 	int	getFailIndex(int index)	const
 	{
-		const Node&	node = nodes[index];
-		return	node.failIndex;
+		const Nd& nd = nds[index];
+		return	nd.failIndex;
 	}
 	int	getDepth(int index)	const
 	{
-		const Node&	node = nodes[index];
-		return	node.depth;
+		const Nd& nd = nds[index];
+		return	nd.depth;
 	}
 	int	getMatchPatternno(int index)	const
 	{
-		const Node&	node = nodes[index];
-		return	node.matchPatternno;
+		const Nd& nd = nds[index];
+		return	nd.matchPatternno;
 	}
 };
