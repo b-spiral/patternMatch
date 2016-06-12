1c04b27b90fbf548d9519d989eb916868350d353
b-spiral <Mitsuhiro.SHIBUYA@gmail.com>
2016-06-12 14:15:17 +0900

    最も単純なアルゴリズムを実装。

--- test/MatcherTest.h
+++ test/MatcherTest.h
@@ -29,18 +29,55 @@ public:
 		}
 	};
 
+private:
+	struct Pattern
+	{
+		std::vector<chr_t>	chrs;
+		int	patternNo;
+		Pattern(const std::vector<chr_t>& chrs_, int patternNo_)
+			: chrs(chrs_), patternNo(patternNo_)
+		{}
+	};
+	std::vector<Pattern>	patterns;
+
 public:
 	// [it,it_e)をpatternNoのパターンとして登録する。
 	void	addPattern( const chr_t* it, const chr_t* it_e, int patternNo)
 	{
-		throw	std::exception();
+		patterns.push_back(Pattern(std::vector<chr_t>(it, it_e), patternNo));
 	}
 
 	// [itChr,itChrE)のchr_t列を登録されているパターン列で分割し、*pResultに書き込む。
 	//	分割は、先頭から貪欲に最長パターン一致で行う。
 	void	matchWhole(std::vector<MatchResult> *pResults, std::vector<chr_t>::const_iterator itChr, std::vector<chr_t>::const_iterator itChrE)	const
 	{
-		throw	std::exception();
+		std::vector<MatchResult>&	results = *pResults;
+		results.clear();
+
+		std::vector<chr_t>::const_iterator itCurChr = itChr;
+		while (itCurChr != itChrE) {
+			const Pattern*	pMatchPattern = nullptr;
+			for (std::vector<Pattern>::const_iterator itPat = patterns.begin(), itPatE = patterns.end(); itPat != itPatE; itPat++) {
+				std::vector<chr_t>::const_iterator it = itCurChr, itP = itPat->chrs.begin(), itPE = itPat->chrs.end();
+				while (itP != itPE && it != itChrE && *it == *itP) {
+					it++;	itP++;
+				}
+				if (itP == itPE) {
+					if (!pMatchPattern || itPat->chrs.size() > pMatchPattern->chrs.size()) {
+						pMatchPattern = &(*itPat);
+					}
+				}
+			}
+			if (pMatchPattern) {
+				size_t size = pMatchPattern->chrs.size();
+				results.push_back(MatchResult(size, pMatchPattern->patternNo));
+				std::advance(itCurChr, size);
+			}
+			else {
+				results.push_back(MatchResult());
+				itCurChr++;
+			}
+		}
 	}
 };
 
