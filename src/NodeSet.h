f9c6b43b9767a578196ba940756b3b9b1565d705
b-spiral <Mitsuhiro.SHIBUYA@gmail.com>
2016-06-13 02:18:56 +0900

    コード構造整理。

--- src/NodeSet.h
+++ src/NodeSet.h
@@ -22,43 +22,95 @@ public:
 		{}
 	};
 
-
 public:
 	static	std::auto_ptr<NodeSet>	buildFromNodeList(const std::vector<Node>& nodes)
 	{
 		return	buildFromNodeList( nullptr, nodes );
 	}
+
 	static	std::auto_ptr<NodeSet>	buildFromNodeList(std::vector<int> *pNodeIndexToNodesetIndex, const std::vector<Node>& nodes)
 	{
 		std::auto_ptr<NodeSet>	pRet(new NodeSet);
 		NodeSet& ns = *pRet;
+
+		std::vector<int>	nodeIdxToNsIdx_body;
+		if (!pNodeIndexToNodesetIndex) {
+			pNodeIndexToNodesetIndex = &nodeIdxToNsIdx_body;
+		}
+		else {
+			pNodeIndexToNodesetIndex->clear();
+		}
+		std::vector<int>&	nodeIdxToNsIdx = *pNodeIndexToNodesetIndex;
 
 		//コンパクトな表現のために以下を行う
 		//	Node.chrToNextIndexのmap<chr_t,int>[]を二分探索可能なvector< {chr_t,int} >を連結したものに置き換える。
 		//	Node.depthをNdに保持しないで済ませるために、Nd[ndIdx]に対応するNode.depthはndIdxに対してソートされた形式にする。
 
 		//nodes[nodeIdx]とns.nds[nsIdx]との対応関係を決める
-		std::vector<int>	nodeIdxToNsIdx_body;
-		if (!pNodeIndexToNodesetIndex) {
-			pNodeIndexToNodesetIndex = &nodeIdxToNsIdx_body;
-		}
-		pNodeIndexToNodesetIndex->clear();
-		std::vector<int>&	nodeIdxToNsIdx = *pNodeIndexToNodesetIndex;
-
-		//nodesのインデックス列を、depthの昇順に並べる。
-		//	インデックス0はルートとして固定するのでソート対象にしない
-		std::vector<int>	nsIdxToNodeIdx(nodes.size(), 0 );
-		for (int i = 0; i < nodes.size(); i++) {
-			nsIdxToNodeIdx[i] = i;
-		}
-		std::sort( ++(nsIdxToNodeIdx.begin()), nsIdxToNodeIdx.end(), Less_NodeIdxDepth_NodeeIdxDepth(nodes) );
+		//	主にindex→depth検索用データのため
+		std::vector<int>	nsIdxToNodeIdx;
+		buildFromNodeList_makeNsIdxOrder( &nsIdxToNodeIdx, nodes );
 
 		nodeIdxToNsIdx.resize(nsIdxToNodeIdx.size());
 		for (int nsIdx = 0; nsIdx < nodes.size(); nsIdx++) {
 			nodeIdxToNsIdx[nsIdxToNodeIdx[nsIdx]] = nsIdx;
 		}
 
-		std::vector<int>&	depthToNsIdx = ns.depthToIdx;
+		//index→depth検索用データを作る
+		ns.setupDepthList(nsIdxToNodeIdx, nodes);
+
+		//nsIdx順にnodeIdxとの対応を変換しながら、map<chr_t,int>[]をvector< {chr_t,int} >に追記していく
+		ns.setupNdList(nsIdxToNodeIdx, nodeIdxToNsIdx, nodes);
+
+		return	pRet;
+	}
+private:
+	static	void	buildFromNodeList_makeNsIdxOrder(std::vector<int> *pNsIdxToNodeIdx, const std::vector<Node>& nodes)
+	{
+		std::vector<int>& nsIdxToNodeIdx = *pNsIdxToNodeIdx;
+		nsIdxToNodeIdx.resize(nodes.size(), 0);
+
+		//nodesのインデックス列を、depthの昇順に並べる。
+		//	インデックス0はルートとして固定するのでソート対象にしない
+		for (int i = 0; i < nodes.size(); i++) {
+			nsIdxToNodeIdx[i] = i;
+		}
+
+		struct Less_NodeIdxDepth_NodeeIdxDepth
+		{
+			const std::vector<Node>& nodes;
+			Less_NodeIdxDepth_NodeeIdxDepth(const std::vector<Node>& nodes_)
+				: nodes(nodes_) {}
+			bool	operator()(int l, int r)	const
+			{
+				int	diff = nodes[l].depth - nodes[r].depth;
+				if (diff != 0) {
+					return	diff < 0;
+				}
+				return	l < r;
+			}
+		};
+		std::sort(++(nsIdxToNodeIdx.begin()), nsIdxToNodeIdx.end(), Less_NodeIdxDepth_NodeeIdxDepth(nodes));
+	}
+
+private:
+	NodeSet()
+	{}
+
+	void	setupDepthList(const std::vector<int>& nsIdxToNodeIdx, const std::vector<Node>& nodes)
+	{
+		struct Less_Depth_NodeIdxDepth
+		{
+			const std::vector<Node>& nodes;
+			Less_Depth_NodeIdxDepth(const std::vector<Node>& nodes_)
+				: nodes(nodes_) {}
+			bool	operator()(int depth, int nodeIdx)	const
+			{
+				return	depth < nodes[nodeIdx].depth;
+			}
+		};
+
+		std::vector<int>&	depthToNsIdx = depthToIdx;
 		depthToNsIdx.clear();
 		for (std::vector<int>::const_iterator itB = nsIdxToNodeIdx.begin(), it = itB, itE = nsIdxToNodeIdx.end(); it != itE; ) {
 			int	depth = nodes[*it].depth;
@@ -70,40 +122,36 @@ public:
 			it = std::upper_bound(it, itE, depth, Less_Depth_NodeIdxDepth(nodes));
 		}
 
-		//nsIdx順にnodeIdxとの対応を変換しながら、map<chr_t,int>[]をvector< {chr_t,int} >に追記していく
+	}
 
+	void	setupNdList(const std::vector<int>& nsIdxToNodeIdx, const std::vector<int>& nodeIdxToNsIdx, const std::vector<Node>& nodes)
+	{
 		{	//無駄な再配置が起こらないように必要量をreserve
 			int	totalEntry = 0;
 			for (int i = 0; i < nodes.size(); i++) {
 				totalEntry += nodes[i].chrToNextIndex.size();
 			}
-			ns.chrIndexs.reserve(totalEntry);
+			chrIndexs.reserve(totalEntry);
 		}
 
-		ns.nds.resize( nodes.size() );
+		nds.resize(nodes.size());
 		for (int nsIdx = 0; nsIdx < nodes.size(); nsIdx++) {
-			Nd& nd = ns.nds[nsIdx];
-			const Node& node = nodes[ nsIdxToNodeIdx[nsIdx] ];
+			Nd& nd = nds[nsIdx];
+			const Node& node = nodes[nsIdxToNodeIdx[nsIdx]];
 
-			nd.chrIndexBegin = ns.chrIndexs.size();
+			nd.chrIndexBegin = chrIndexs.size();
 			for (std::map<chr_t, int>::const_iterator it = node.chrToNextIndex.begin(), itE = node.chrToNextIndex.end(); it != itE; it++) {
 				ChrIndex	ci;
 				ci.chr = it->first;
 				ci.i = nodeIdxToNsIdx[it->second];
-				ns.chrIndexs.push_back(ci);
+				chrIndexs.push_back(ci);
 			}
 
 			nd.failIndex = node.failIndex<0 ? -1 : nodeIdxToNsIdx[node.failIndex];
 			nd.matchIndex = node.matchIndex<0 ? -1 : nodeIdxToNsIdx[node.matchIndex];
 			nd.matchPatternno = node.matchPatternno;
 		}
-
-		return	pRet;
 	}
-
-private:
-	NodeSet()
-	{}
 
 private:
 	struct ChrIndex
@@ -113,34 +161,6 @@ private:
 	};
 	std::vector<ChrIndex>	chrIndexs;
 	
-	struct Less_ChrIdxChr_Chr
-	{
-		inline	bool	operator()(const ChrIndex& ci, chr_t chr)	const	{	return	ci.chr < chr;	}
-	};
-	struct Less_NodeIdxDepth_NodeeIdxDepth
-	{
-		const std::vector<Node>& nodes;
-		Less_NodeIdxDepth_NodeeIdxDepth(const std::vector<Node>& nodes_)
-			: nodes(nodes_) {}
-		bool	operator()(int l, int r)	const
-		{
-			int	diff = nodes[l].depth - nodes[r].depth;
-			if (diff != 0) {
-				return	diff < 0;
-			}
-			return	l < r;
-		}
-	};
-	struct Less_Depth_NodeIdxDepth
-	{
-		const std::vector<Node>& nodes;
-		Less_Depth_NodeIdxDepth(const std::vector<Node>& nodes_)
-			: nodes(nodes_) {}
-		bool	operator()(int depth, int nodeIdx )	const
-		{
-			return	depth < nodes[nodeIdx].depth;
-		}
-	};
 	struct Nd
 	{
 		int	chrIndexBegin;
@@ -148,7 +168,7 @@ private:
 		int	failIndex;
 		int	matchIndex;
 	};
-	std::vector<Nd>	nds;
+	std::vector<Nd>		nds;
 	std::vector<int>	depthToIdx;
 
 public:
@@ -164,6 +184,11 @@ public:
 		}else{
 			itE = chrIndexs.end();
 		}
+
+		struct Less_ChrIdxChr_Chr
+		{
+			inline	bool	operator()(const ChrIndex& ci, chr_t chr)	const { return	ci.chr < chr; }
+		};
 		std::vector<ChrIndex>::const_iterator it = std::lower_bound(itB, itE, chr, Less_ChrIdxChr_Chr());
 		if ( it != itE && it->chr == chr) {
 			return	std::make_pair(true, it->i);
